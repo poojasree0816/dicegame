61f0b9285e8b5cd35bcb4cda595cb65f76f000bf
Poojasree S <poojasree0816@gmail.com>
2020-07-18 19:49:43 +0530

    Add files via upload
    this is a very simple basic dice game

---
+++ dice.js
@@ -0,0 +1,43 @@
+var numondice1= Math.floor(((Math.random())*6)+1);
+var numondice2= Math.floor(((Math.random())*6)+1);
+switch(numondice1){
+  case 1:
+    document.querySelector(".img1").setAttribute("src","images/dice1.png");
+    break;
+  case 2:
+    document.querySelector(".img1").setAttribute("src","images/dice2.png");
+    break;
+  case 3:
+    document.querySelector(".img1").setAttribute("src","images/dice3.png");
+    break;
+  case 4:
+    document.querySelector(".img1").setAttribute("src","images/dice4.png");
+    break;
+  case 5:
+    document.querySelector(".img1").setAttribute("src","images/dice5.png");
+
+}
+
+switch(numondice2){
+  case 1:
+    document.querySelector(".img2").setAttribute("src","images/dice1.png");
+    break;
+  case 2:
+    document.querySelector(".img2").setAttribute("src","images/dice2.png");
+    break;
+  case 3:
+    document.querySelector(".img2").setAttribute("src","images/dice3.png");
+    break;
+  case 4:
+    document.querySelector(".img2").setAttribute("src","images/dice4.png");
+    break;
+  case 5:
+    document.querySelector(".img2").setAttribute("src","images/dice5.png");
+}
+if(numondice1>numondice2){
+  document.querySelector("h1").innerHTML="Player1 wins";
+}
+else if (numondice2>numondice1) {
+  document.querySelector("h1").innerHTML="player2 wins";
+
+}
